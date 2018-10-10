e6c940f57c59c3df170225c8b21261dd254fe813
Matthew Iverson <matthewiver@protonmail.com>
2018-10-10 18:05:58 -0500

    change media card to my liking but not proper sizing

--- src/Components/UI/ImgMediaCard.js
+++ src/Components/UI/ImgMediaCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -9,25 +9,14 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    // ⚠️ object-fit is not supported by IE11.
-    objectFit: 'cover',
-  },
-};
-
-function ImgMediaCard(props) {
-  const { classes } = props;
+class ImgMediaCard extends Component {
+  render() {
   return (
-    <Card className={classes.card}>
+    <Card>
       <CardActionArea>
         <CardMedia
           component="img"
           alt={this.props.alt}
-          className={classes.media}
           height="140"
           image={this.props.image}
           title={this.props.title}
@@ -51,12 +40,9 @@ function ImgMediaCard(props) {
     </Card>
   );
 }
+}
 
-ImgMediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(ImgMediaCard);
+export default (ImgMediaCard);
 
 /*
 <ImgMediaCard
