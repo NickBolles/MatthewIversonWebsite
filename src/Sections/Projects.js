0ffe661d534c155dfd41dd9550b3ff8076e846bb
Matthew Iverson <matthewiver@protonmail.com>
2018-10-10 10:59:05 -0500

    idea of how projects will look

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -1,6 +1,12 @@
+import Projects from "../Components/UI/ImgMediaCard";
+import React form "react";
 /* 
 Projects
 
 3 x 3 
 show more
 */
+
+<Projects />
+        <Projects />
+        <Projects />
