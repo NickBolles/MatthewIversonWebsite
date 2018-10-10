1e7742222c042ff1c3b5d47be2ce76b4345bd200
Matthew Iverson <matthewiver@protonmail.com>
2018-10-10 13:12:23 -0500

    rename

--- src/Sections/Biography.js
+++ src/Sections/Biography.js
@@ -1,17 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+import Paper from "@material-ui/core/Paper";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Paper from "@material-ui/core/Paper";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   root: {
     width: "70%",
     margin: "auto"
+  },
+  appBar: {
+    position: "relative"
+  },
+  flex: {
+    flex: 1
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
@@ -19,75 +32,126 @@ const styles = theme => ({
   }
 });
 
-function Biography(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <section>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}>
-              Matthew Iverson has an unique and valuable vision towards how he
-              lives. He builds his ideas out beautifully, typically over the
-              doubts and opposition of others. Matthew has solidified his
-              approach to life with the philosophy of stoicism. He loves
-              bringing his ideas to fruition to help aid others in their lives.
-              Matthew has always had a sense of entrepreneurship and creativity
-              in his veins because of his mother who taught him all about them
-              from a young age. His Father taught him about how to deliver on a
-              deal and knowledge of the importance of being able to have a
-              analytical and business mind. Together his parents taught him the
-              foundation of how to create his vision to thrive in business.
-              Matthew loves creating, guiding and inspiring by his actions
-              rather than forcing others into boxes with fear, how other
-              “leaders” create the culture in their business. Matthew strives to
-              create a better existences for all of us.
-            </Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              LONG BIO!
-              <Paper>
-                Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate
-                velit malesuada posuere dis cras, nec vehicula magna cursus
-                semper sapien eros dictumst praesent commodo fermentum. Ligula
-                rhoncus nulla elementum pretium euismod dictumst odio suscipit
-                faucibus nisi magnis, neque ullamcorper aptent ultrices viverra
-                phasellus nullam interdum quam. Vehicula sollicitudin ridiculus
-                aliquet libero eleifend cum arcu, facilisi ac sociosqu consequat
-                senectus aliquam id erat, suspendisse orci praesent tellus ante
-                ultricies. Porttitor odio semper convallis libero magna et
-                facilisis erat, id hendrerit pulvinar blandit feugiat parturient
-                natoque, nullam senectus commodo rhoncus lobortis inceptos sed.
-                Feugiat sed justo quis montes dis massa accumsan rutrum ad
-                luctus aenean augue vel posuere inceptos, ligula molestie curae
-                metus cubilia morbi nibh donec sodales platea sagittis faucibus
-                semper elementum. Aptent scelerisque semper vitae augue magna
-                torquent praesent nascetur fringilla, per sapien mi quam
-                porttitor sodales condimentum justo, litora tempus sed vel
-                accumsan placerat urna potenti.Lorem ipsum dolor sit amet
-                consectetur adipiscing elit vulputate velit malesuada posuere
-                dis cras, nec vehicula magna cursus semper sapien eros dictumst
-                praesent commodo fermentum. Ligula rhoncus nulla elementum
-                pretium euismod dictumst odio suscipit faucibus nisi magnis,
-                neque ullamcorper aptent ultrices viverra phasellus nullam
-                interdum quam. Vehicula sollicitudin ridiculus aliquet libero
-                eleifend cum arcu, facilisi ac sociosqu consequat senectus
-                aliquam id erat, suspendisse orci praesent tellus ante
-                ultricies. Porttitor odio semper convallis libero magna et
-                facilisis erat, id hendrerit pulvinar blandit feugiat parturient
-                natoque, nullam senectus commodo rhoncus lobortis inceptos sed.
-                Feugiat sed justo quis montes dis massa accumsan rutrum ad
-                luctus aenean augue vel posuere inceptos, ligula molestie curae
-                metus cubilia morbi nibh donec sodales platea sagittis faucibus
-                semper elementum. Aptent
-              </Paper>
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-      </section>
-    </div>
-  );
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
+
+class Biography extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Button onClick={this.handleClickOpen}>Auto Biography</Button>
+        <Dialog
+          fullScreen
+          open={this.state.open}
+          onClose={this.handleClose}
+          TransitionComponent={Transition}
+        >
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                onClick={this.handleClose}
+                aria-label="Close"
+              >
+                <CloseIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" className={classes.flex}>
+                AutoBiography
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Typography>
+            <div className={classes.root}>
+              <section>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>
+                      Matthew Iverson has an unique and valuable vision towards
+                      how he lives. He builds his ideas out beautifully,
+                      typically over the doubts and opposition of others.
+                      Matthew has solidified his approach to life with the
+                      philosophy of stoicism. He loves bringing his ideas to
+                      fruition to help aid others in their lives. Matthew has
+                      always had a sense of entrepreneurship and creativity in
+                      his veins because of his mother who taught him all about
+                      them from a young age. His Father taught him about how to
+                      deliver on a deal and knowledge of the importance of being
+                      able to have a analytical and business mind. Together his
+                      parents taught him the foundation of how to create his
+                      vision to thrive in business. Matthew loves creating,
+                      guiding and inspiring by his actions rather than forcing
+                      others into boxes with fear, how other “leaders” create
+                      the culture in their business. Matthew strives to create a
+                      better existences for all of us.
+                    </Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Typography>
+                      LONG BIO!
+                      <Paper>
+                        Lorem ipsum dolor sit amet consectetur adipiscing elit
+                        vulputate velit malesuada posuere dis cras, nec vehicula
+                        magna cursus semper sapien eros dictumst praesent
+                        commodo fermentum. Ligula rhoncus nulla elementum
+                        pretium euismod dictumst odio suscipit faucibus nisi
+                        magnis, neque ullamcorper aptent ultrices viverra
+                        phasellus nullam interdum quam. Vehicula sollicitudin
+                        ridiculus aliquet libero eleifend cum arcu, facilisi ac
+                        sociosqu consequat senectus aliquam id erat, suspendisse
+                        orci praesent tellus ante ultricies. Porttitor odio
+                        semper convallis libero magna et facilisis erat, id
+                        hendrerit pulvinar blandit feugiat parturient natoque,
+                        nullam senectus commodo rhoncus lobortis inceptos sed.
+                        Feugiat sed justo quis montes dis massa accumsan rutrum
+                        ad luctus aenean augue vel posuere inceptos, ligula
+                        molestie curae metus cubilia morbi nibh donec sodales
+                        platea sagittis faucibus semper elementum. Aptent
+                        scelerisque semper vitae augue magna torquent praesent
+                        nascetur fringilla, per sapien mi quam porttitor sodales
+                        condimentum justo, litora tempus sed vel accumsan
+                        placerat urna potenti.Lorem ipsum dolor sit amet
+                        consectetur adipiscing elit vulputate velit malesuada
+                        posuere dis cras, nec vehicula magna cursus semper
+                        sapien eros dictumst praesent commodo fermentum. Ligula
+                        rhoncus nulla elementum pretium euismod dictumst odio
+                        suscipit faucibus nisi magnis, neque ullamcorper aptent
+                        ultrices viverra phasellus nullam interdum quam.
+                        Vehicula sollicitudin ridiculus aliquet libero eleifend
+                        cum arcu, facilisi ac sociosqu consequat senectus
+                        aliquam id erat, suspendisse orci praesent tellus ante
+                        ultricies. Porttitor odio semper convallis libero magna
+                        et facilisis erat, id hendrerit pulvinar blandit feugiat
+                        parturient natoque, nullam senectus commodo rhoncus
+                        lobortis inceptos sed. Feugiat sed justo quis montes dis
+                        massa accumsan rutrum ad luctus aenean augue vel posuere
+                        inceptos, ligula molestie curae metus cubilia morbi nibh
+                        donec sodales platea sagittis faucibus semper elementum.
+                        Aptent
+                      </Paper>
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              </section>
+            </div>
+          </Typography>
+        </Dialog>
+      </div>
+    );
+  }
 }
 
 Biography.propTypes = {
