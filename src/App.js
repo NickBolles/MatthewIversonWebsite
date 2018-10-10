233a1c9a5c9f97e59cd63f791a232b0ee6ae38f7
Matthew Iverson <matthewiver@protonmail.com>
2018-10-09 22:54:07 -0500

    prettier

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import PrimarySearchAppBar from "./Components/UI/PrimarySearchAppBar";
 import SimpleExpansionPanel from "./Components/UI/SimpleExpansionPanel";
 import ProfileIntro from "./Components/UI/ProfileIntro";
+import ImgMediaCard from "./Components/UI/ImgMediaCard";
 
 class App extends Component {
   render() {
@@ -15,35 +16,106 @@ class App extends Component {
           <ProfileIntro />
         </section>
         <section id="Projects">
-        Projects
+          <ImgMediaCard />
         </section>
         <section id="Bio">
           <SimpleExpansionPanel />
         </section>
         <section id="Books">
-        Favorite Books
-        <div>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate velit malesuada posuere dis cras, nec vehicula magna cursus semper sapien eros dictumst praesent commodo fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst odio suscipit faucibus nisi magnis, neque ullamcorper aptent ultrices viverra phasellus nullam interdum quam. Vehicula sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam id erat, suspendisse orci praesent tellus ante ultricies.
-
-Porttitor odio semper convallis libero magna et facilisis erat, id hendrerit pulvinar blandit feugiat parturient natoque, nullam senectus commodo rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis massa accumsan rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie curae metus cubilia morbi nibh donec sodales platea sagittis faucibus semper elementum. Aptent scelerisque semper vitae augue magna torquent praesent nascetur fringilla, per sapien mi quam porttitor sodales condimentum justo, litora tempus sed vel accumsan placerat urna potenti.Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate velit malesuada posuere dis cras, nec vehicula magna cursus semper sapien eros dictumst praesent commodo fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst odio suscipit faucibus nisi magnis, neque ullamcorper aptent ultrices viverra phasellus nullam interdum quam. Vehicula sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam id erat, suspendisse orci praesent tellus ante ultricies.
-
-Porttitor odio semper convallis libero magna et facilisis erat, id hendrerit pulvinar blandit feugiat parturient natoque, nullam senectus commodo rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis massa accumsan rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie curae metus cubilia morbi nibh donec sodales platea sagittis faucibus semper elementum. Aptent scelerisque semper vitae augue magna torquent praesent nascetur fringilla, per sapien mi quam porttitor sodales condimentum justo, litora tempus sed vel accumsan placerat urna potenti.Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate velit malesuada posuere dis cras, nec vehicula magna cursus semper sapien eros dictumst praesent commodo fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst odio suscipit faucibus nisi magnis, neque ullamcorper aptent ultrices viverra phasellus nullam interdum quam. Vehicula sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam id erat, suspendisse orci praesent tellus ante ultricies.
-
-Porttitor odio semper convallis libero magna et facilisis erat, id hendrerit pulvinar blandit feugiat parturient natoque, nullam senectus commodo rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis massa accumsan rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie curae metus cubilia morbi nibh donec sodales platea sagittis faucibus semper elementum. Aptent scelerisque semper vitae augue magna torquent praesent nascetur fringilla, per sapien mi quam porttitor sodales condimentum justo, litora tempus sed vel accumsan placerat urna potenti.
-        </div>
+          Favorite Books
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate
+            velit malesuada posuere dis cras, nec vehicula magna cursus semper
+            sapien eros dictumst praesent commodo fermentum. Ligula rhoncus
+            nulla elementum pretium euismod dictumst odio suscipit faucibus nisi
+            magnis, neque ullamcorper aptent ultrices viverra phasellus nullam
+            interdum quam. Vehicula sollicitudin ridiculus aliquet libero
+            eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam
+            id erat, suspendisse orci praesent tellus ante ultricies. Porttitor
+            odio semper convallis libero magna et facilisis erat, id hendrerit
+            pulvinar blandit feugiat parturient natoque, nullam senectus commodo
+            rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis
+            massa accumsan rutrum ad luctus aenean augue vel posuere inceptos,
+            ligula molestie curae metus cubilia morbi nibh donec sodales platea
+            sagittis faucibus semper elementum. Aptent scelerisque semper vitae
+            augue magna torquent praesent nascetur fringilla, per sapien mi quam
+            porttitor sodales condimentum justo, litora tempus sed vel accumsan
+            placerat urna potenti.Lorem ipsum dolor sit amet consectetur
+            adipiscing elit vulputate velit malesuada posuere dis cras, nec
+            vehicula magna cursus semper sapien eros dictumst praesent commodo
+            fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst
+            odio suscipit faucibus nisi magnis, neque ullamcorper aptent
+            ultrices viverra phasellus nullam interdum quam. Vehicula
+            sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac
+            sociosqu consequat senectus aliquam id erat, suspendisse orci
+            praesent tellus ante ultricies. Porttitor odio semper convallis
+            libero magna et facilisis erat, id hendrerit pulvinar blandit
+            feugiat parturient natoque, nullam senectus commodo rhoncus lobortis
+            inceptos sed. Feugiat sed justo quis montes dis massa accumsan
+            rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie
+            curae metus cubilia morbi nibh donec sodales platea sagittis
+            faucibus semper elementum. Aptent scelerisque semper vitae augue
+            magna torquent praesent nascetur fringilla, per sapien mi quam
+            porttitor sodales condimentum justo, litora tempus sed vel accumsan
+            placerat urna potenti.Lorem ipsum dolor sit amet consectetur
+            adipiscing elit vulputate velit malesuada posuere dis cras, nec
+            vehicula magna cursus semper sapien eros dictumst praesent commodo
+            fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst
+            odio suscipit faucibus nisi magnis, neque ullamcorper aptent
+            ultrices viverra phasellus nullam interdum quam. Vehicula
+            sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac
+            sociosqu consequat senectus aliquam id erat, suspendisse orci
+            praesent tellus ante ultricies. Porttitor odio semper convallis
+            libero magna et facilisis erat, id hendrerit pulvinar blandit
+            feugiat parturient natoque, nullam senectus commodo rhoncus lobortis
+            inceptos sed. Feugiat sed justo quis montes dis massa accumsan
+            rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie
+            curae metus cubilia morbi nibh donec sodales platea sagittis
+            faucibus semper elementum. Aptent scelerisque semper vitae augue
+            magna torquent praesent nascetur fringilla, per sapien mi quam
+            porttitor sodales condimentum justo, litora tempus sed vel accumsan
+            placerat urna potenti.
+          </div>
         </section>
         <section id="Contact">
-        Contact
-        <div>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate velit malesuada posuere dis cras, nec vehicula magna cursus semper sapien eros dictumst praesent commodo fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst odio suscipit faucibus nisi magnis, neque ullamcorper aptent ultrices viverra phasellus nullam interdum quam. Vehicula sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam id erat, suspendisse orci praesent tellus ante ultricies.
-
-Porttitor odio semper convallis libero magna et facilisis erat, id hendrerit pulvinar blandit feugiat parturient natoque, nullam senectus commodo rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis massa accumsan rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie curae metus cubilia morbi nibh donec sodales platea sagittis faucibus semper elementum. Aptent scelerisque semper vitae augue magna torquent praesent nascetur fringilla, per sapien mi quam porttitor sodales condimentum justo, litora tempus sed vel accumsan placerat urna potenti.
-Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate velit malesuada posuere dis cras, nec vehicula magna cursus semper sapien eros dictumst praesent commodo fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst odio suscipit faucibus nisi magnis, neque ullamcorper aptent ultrices viverra phasellus nullam interdum quam. Vehicula sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam id erat, suspendisse orci praesent tellus ante ultricies.
-
-Porttitor odio semper convallis libero magna et facilisis erat, id hendrerit pulvinar blandit feugiat parturient natoque, nullam senectus commodo rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis massa accumsan rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie curae metus cubilia morbi nibh donec sodales platea sagittis faucibus semper elementum. Aptent scelerisque semper vitae augue magna torquent praesent nascetur fringilla, per sapien mi quam porttitor sodales condimentum justo, litora tempus sed vel accumsan placerat urna potenti.
-        </div>
+          Contact
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit vulputate
+            velit malesuada posuere dis cras, nec vehicula magna cursus semper
+            sapien eros dictumst praesent commodo fermentum. Ligula rhoncus
+            nulla elementum pretium euismod dictumst odio suscipit faucibus nisi
+            magnis, neque ullamcorper aptent ultrices viverra phasellus nullam
+            interdum quam. Vehicula sollicitudin ridiculus aliquet libero
+            eleifend cum arcu, facilisi ac sociosqu consequat senectus aliquam
+            id erat, suspendisse orci praesent tellus ante ultricies. Porttitor
+            odio semper convallis libero magna et facilisis erat, id hendrerit
+            pulvinar blandit feugiat parturient natoque, nullam senectus commodo
+            rhoncus lobortis inceptos sed. Feugiat sed justo quis montes dis
+            massa accumsan rutrum ad luctus aenean augue vel posuere inceptos,
+            ligula molestie curae metus cubilia morbi nibh donec sodales platea
+            sagittis faucibus semper elementum. Aptent scelerisque semper vitae
+            augue magna torquent praesent nascetur fringilla, per sapien mi quam
+            porttitor sodales condimentum justo, litora tempus sed vel accumsan
+            placerat urna potenti. Lorem ipsum dolor sit amet consectetur
+            adipiscing elit vulputate velit malesuada posuere dis cras, nec
+            vehicula magna cursus semper sapien eros dictumst praesent commodo
+            fermentum. Ligula rhoncus nulla elementum pretium euismod dictumst
+            odio suscipit faucibus nisi magnis, neque ullamcorper aptent
+            ultrices viverra phasellus nullam interdum quam. Vehicula
+            sollicitudin ridiculus aliquet libero eleifend cum arcu, facilisi ac
+            sociosqu consequat senectus aliquam id erat, suspendisse orci
+            praesent tellus ante ultricies. Porttitor odio semper convallis
+            libero magna et facilisis erat, id hendrerit pulvinar blandit
+            feugiat parturient natoque, nullam senectus commodo rhoncus lobortis
+            inceptos sed. Feugiat sed justo quis montes dis massa accumsan
+            rutrum ad luctus aenean augue vel posuere inceptos, ligula molestie
+            curae metus cubilia morbi nibh donec sodales platea sagittis
+            faucibus semper elementum. Aptent scelerisque semper vitae augue
+            magna torquent praesent nascetur fringilla, per sapien mi quam
+            porttitor sodales condimentum justo, litora tempus sed vel accumsan
+            placerat urna potenti.
+          </div>
         </section>
-       
       </div>
     );
   }
