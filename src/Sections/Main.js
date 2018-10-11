8206dff2df79981e985aa0fb451619fa36cc4d36
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 17:21:03 -0500

    true landing page

--- src/Sections/Main.js
+++ src/Sections/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import App from '../Components/UI/ProfileIntro';
+import App from './ProfileIntro';
 import Programming from "./Programming";
 import PentrationTesting from "./PentrationTesting";
 import Contact from './Contact';
