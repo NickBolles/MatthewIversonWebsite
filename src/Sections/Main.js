8c44c4ca9b261480e2a2000ac5815290b974073c
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 14:48:39 -0500

    main.js for navigation

---
+++ src/Sections/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import App from './App';
+import Programming from "./Programming";
+import PentrationTesting from "./PentrationTesting";
+import Contact from './Contact';
+import Resume from './Resume';
+import Biography from './Biography';
+
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={App} />
+    <Route path="/Biography" component={Biography} />
+    <Route path="/Resume" component={Resume} />
+    <Route path="/Programming" component={Programming} />
+    <Route path="/PentrationTesting" component={PentrationTesting} />
+    <Route path="/Contact" component={Contact} />
+  </Switch>
+)
+
+export default Main;
