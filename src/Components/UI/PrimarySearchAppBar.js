bd94e4676cc999c8814d48fc815dfe6ff99b30d2
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 06:02:30 -0500

    remove non used icon

--- src/Components/UI/PrimarySearchAppBar.js
+++ src/Components/UI/PrimarySearchAppBar.js
@@ -10,7 +10,6 @@ import MailIcon from "@material-ui/icons/Mail";
 import ComputerIcon from "@material-ui/icons/Computer";
 import InsertDriveFile from "@material-ui/icons/InsertDriveFile";
 import WorkIcon from "@material-ui/icons/Work";
-import ImportContacts from "@material-ui/icons/ImportContacts";
 import Button from "@material-ui/core/Button";
 
 const styles = theme => ({
@@ -72,9 +71,6 @@ class PrimarySearchAppBar extends React.Component {
               </IconButton>
               <IconButton color="inherit">
                 <SecurityIcon />
-              </IconButton>
-              <IconButton color="inherit">
-                <ImportContacts />
               </IconButton>
               <IconButton color="inherit">
                 <MailIcon />
