cd58222c199460cf6c5b26c9268d7118a17a49fe
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 14:45:54 -0500

    descriptive

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
+        <section>
           <PrimarySearchAppBar />
-        </div>
+        </section>
         <section id="Profile">
           <ProfileIntro />
         </section>
