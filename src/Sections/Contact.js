1eca42137654862fc07381aa629d86690d923af2
Matthew Iverson <matthewiver@protonmail.com>
2018-10-10 21:52:12 -0500

    prettier

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
-import { List, ListItem, ListItemIcon, Grid} from "@material-ui/core"
-
+import React, { Component } from "react";
+import { ListItem, ListItemIcon, Grid } from "@material-ui/core";
 
 class Contact extends Component {
   render() {
-    return(
+    return (
       <div className="contact-body">
-        <Grid className="contact-grid">
-          <Grid xs={6}>
-          </Grid>
-          <Grid xs={6}>
+        <Grid className="contact-grid" >
+          <Grid xs={3}>
             <h2>Contact Me</h2>
-            <hr/>
+            <hr />
             <div className="contact-list">
-                <ListItem>
-                  <ListItemIcon style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    matthewiver@protonmail.com
-                  </ListItemIcon>
-                </ListItem>     
-                <ListItem>
-                  <ListItemIcon style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-skype" aria-hidden="true"/>
-                    MySkypeID
-                  </ListItemIcon>
-                </ListItem>
-              </List>
+              <ListItem>
+                <ListItemIcon style={{ fontSize: "30px", fontFamily: "Anton" }}>
+                  <i className="fa fa-envelope" aria-hidden="true" />
+                  matthewiver@protonmail.com
+                </ListItemIcon>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon style={{ fontSize: "30px", fontFamily: "Anton" }}>
+                  <i className="fa fa-skype" aria-hidden="true" />
+                  MySkypeID
+                </ListItemIcon>
+              </ListItem>
             </div>
           </Grid>
         </Grid>
       </div>
-    )
+    );
   }
 }
 
