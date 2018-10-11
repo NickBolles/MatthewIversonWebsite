1ef99de0bdb9766ff7e7393feac93d5a3c0c22d2
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 05:31:01 -0500

    update book pages

--- src/Components/UI/GridLists/EntrBooks.js
+++ src/Components/UI/GridLists/EntrBooks.js
@@ -1,69 +1,90 @@
 const EntrBooks = [
-    {
-      img: 'https://vavamr.files.wordpress.com/2013/04/rich-dad-poor-dad-cashflow-quadrant.png',
-      title: 'Rich Dad Poor Dad',
-      author: 'Robert Kyosoki',
-      cols: 2,
-      featured: true,
-    },
-    {
-      img: 'http://www.brandhome.com/wp-content/uploads/2015/05/TIPPINGPOINT_1280x640.jpg',
-      title: 'The Tipping Point',
-      author: 'Malcolm Gladwell',
-    },
-    {
-      img: 'http://pxhst.co/avaxhome/79/09/00350979_medium.jpeg',
-      title: 'A Guide to the Good Life',
-      author: 'William B. Irvine',
-    },
-    {
-      img: 'https://is3-ssl.mzstatic.com/image/thumb/Music6/v4/e8/44/25/e8442553-5383-e136-8c90-87a4c3e60d40/source/1200x630bf.jpg',
-      title: 'Extreme Ownership',
-      author: 'Jocko Willink, Leif Babin',
-      featured: true,
-    },
-    {
-      img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1486492516i/34189313._UY630_SR1200,630_.jpg',
-      title: 'The 5 Second Rule',
-      author: 'Mel Robbins',
-    },
-    {
-      img: 'https://cdn.cliqueinc.com/posts/188407/7-must-read-career-books-thatll-get-you-the-joband-change-your-life-1713433-1459292669.640x0c.jpg',
-      title: 'Deep Work',
-      author: 'Cal Newport',
-    },
-    {
-      img: '/static/images/grid-list/vegetables.jpg',
-      title: 'Vegetables',
-      author: 'jill111',
-      cols: 2,
-    },
-    {
-      img: '/static/images/grid-list/plant.jpg',
-      title: 'Water plant',
-      author: 'BkrmadtyaKarki',
-    },
-    {
-      img: '/static/images/grid-list/mushroom.jpg',
-      title: 'Mushrooms',
-      author: 'PublicDomainPictures',
-    },
-    {
-      img: '/static/images/grid-list/olive.jpg',
-      title: 'Olive oil',
-      author: 'congerdesign',
-    },
-    {
-      img: '/static/images/grid-list/star.jpg',
-      title: 'Sea star',
-      cols: 2,
-      author: '821292',
-    },
-    {
-      img: '/static/images/grid-list/bike.jpg',
-      title: 'Bike',
-      author: 'danfador',
-    },
-  ];
-  
-  export default EntrBooks;
+  {
+    img:
+      "https://vavamr.files.wordpress.com/2013/04/rich-dad-poor-dad-cashflow-quadrant.png",
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kyosoki",
+    cols: 2,
+    featured: true
+  },
+  {
+    img:
+      "http://www.brandhome.com/wp-content/uploads/2015/05/TIPPINGPOINT_1280x640.jpg",
+    title: "The Tipping Point",
+    author: "Malcolm Gladwell"
+  },
+  {
+    img: "http://pxhst.co/avaxhome/79/09/00350979_medium.jpeg",
+    title: "A Guide to the Good Life",
+    author: "William B. Irvine"
+  },
+  {
+    img:
+      "https://is3-ssl.mzstatic.com/image/thumb/Music6/v4/e8/44/25/e8442553-5383-e136-8c90-87a4c3e60d40/source/1200x630bf.jpg",
+    title: "Extreme Ownership",
+    author: "Jocko Willink, Leif Babin",
+    featured: true
+  },
+  {
+    img:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1486492516i/34189313._UY630_SR1200,630_.jpg",
+    title: "The 5 Second Rule",
+    author: "Mel Robbins"
+  },
+  {
+    img:
+      "https://cdn.cliqueinc.com/posts/188407/7-must-read-career-books-thatll-get-you-the-joband-change-your-life-1713433-1459292669.640x0c.jpg",
+    title: "Deep Work",
+    author: "Cal Newport"
+  },
+  {
+    img:
+      "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.freebooks.com%2Fwp-content%2Fuploads%2F2013%2F04%2FThe-Art-of-War.jpg&f=1",
+    title: "Art of War",
+    author: "Sun Tzu",
+    cols: 2
+  },
+  {
+    img: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zp-2Vu8K0PD5133k3KlhqQHaF7%26pid%3D15.1&f=1",
+    title: "Outliers",
+    author: "Malclom Gladwell"
+  },
+  {
+    img: "http://www.babson.edu/admission/undergraduate/new-students/PublishingImages/the-power-of-habit.jpg",
+    title: "The Power of Habit",
+    author: "Charles Duhigg"
+  },
+  {
+    img: "http://d20eq91zdmkqd.cloudfront.net/assets/images/book/large/9781/4736/9781473610385.jpg",
+    title: "Business Adventures",
+    author: "John Brooks"
+  },
+  {
+    img: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fecx.images-amazon.com%2Fimages%2FI%2F61AQzauI9zL._SL600_.jpg&f=1",
+    title: "Benjamin Franklin",
+    cols: 2,
+    author: "Walter Isaacson"
+  },
+  {
+    img: "http://covers4.booksamillion.com/covers/bam/1/51/977/285/1519772858.jpg",
+    title: "Habits of the Super Rich",
+    author: "Bruce Walker"
+  },
+  {
+    img: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.gr-assets.com%2Fimages%2FS%2Fcompressed.photo.goodreads.com%2Fbooks%2F1347711639i%2F3828382._UY630_SR1200%2C630_.jpg&f=1",
+    title: "Tribes",
+    author: "Seth Godin"
+  },
+  {
+    img: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftarget.scene7.com%2Fis%2Fimage%2FTarget%2F11995414%3Fwid%3D520%26hei%3D520%26fmt%3Dpjpeg&f=1",
+    title: "Switch",
+    author: "Dan Heath, Chip Heath"
+  },
+  {
+    img: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Ftabs.web.media%2Fb%2Fh%2Fbhyr%2Fbhyr-square-orig.jpg&f=1",
+    title: "Elon Musk",
+    author: "Ashlee Vance"
+  }
+];
+
+export default EntrBooks;
