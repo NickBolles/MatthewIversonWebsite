7d01048427e05c3b9d5a65637f679d550c158fd0
Matthew Iverson <matthewiver@protonmail.com>
2018-10-12 15:40:00 -0500

    change grid

--- src/Components/UI/NestedGrid.js
+++ src/Components/UI/NestedGrid.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import DummyImgMediaCard from "./DummyImgMediaCard";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing.unit,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+    textAlign: "center",
+    color: theme.palette.text.secondary
+  }
 });
 
 function FormRow(props) {
@@ -21,20 +22,21 @@ function FormRow(props) {
   return (
     <React.Fragment>
       <Grid item xs={4}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>item</Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>item</Paper>
+        <Paper className={classes.paper}>
+          <DummyImgMediaCard
+            image="../Pictures/MyWebsite.png"
+            alt="My First Website"
+            title="My First Website"
+            description="I created my first project independently within 5 days which is the website you are looking at currently."
+          />
+        </Paper>
       </Grid>
     </React.Fragment>
   );
 }
 
 FormRow.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 function NestedGrid(props) {
@@ -46,19 +48,28 @@ function NestedGrid(props) {
         <Grid item xs={12} container spacing={24}>
           <FormRow classes={classes} />
         </Grid>
-        <Grid item xs={12} container spacing={24}>
-          <FormRow classes={classes} />
-        </Grid>
-        <Grid item xs={12} container spacing={24}>
-          <FormRow classes={classes} />
-        </Grid>
       </Grid>
     </div>
   );
 }
 
 NestedGrid.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(NestedGrid);
+
+/* EXAMPLE
+
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+          <DummyImgMediaCard
+            image="LINKtowebsite"
+            alt="alternate name if image is not available"
+            title="Name"
+            description="descriptive sentences on the project and how it was created"
+          />
+        </Paper>
+      </Grid>
+
+*/
