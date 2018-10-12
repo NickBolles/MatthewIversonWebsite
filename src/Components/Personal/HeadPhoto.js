b5318c80fdb60f9a7cf94dbc67ac7ee63cb46bb8
Matthew Iverson <matthewiver@protonmail.com>
2018-10-12 13:25:21 -0500

    individual headshot

---
+++ src/Components/Personal/HeadPhoto.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HeadPhoto extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+          alt="avatar"
+          className="avatar-img"
+        />
+      </div>
+    );
+  }
+}
+
+export default HeadPhoto;
