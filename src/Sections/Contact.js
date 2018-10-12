e7066d6f4f48c69f300276d02f23ed76645a1e75
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 20:43:54 -0500

    minimize

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -1,48 +1,21 @@
 import React, { Component } from "react";
-import { ListItem, ListItemIcon, Grid } from "@material-ui/core";
+import { Typography, Paper } from "@material-ui/core";
 
 class Contact extends Component {
   render() {
     return (
-      <div class="footer-below">
-      <div class="container">
-          <Grid className="contact-grid">
-            <Grid xs={3}>
-              <h2>Contact Me</h2>
-              <hr />
-              <div className="contact-list">
-                <ListItem>
-                  <ListItemIcon
-                    style={{ fontSize: "30px", fontFamily: "Anton" }}
-                  >
-                    <i /*email icon */ />
-                    <div className="ListItem">
-                    matthewiver@protonmail.com
-                    </div>
-                  </ListItemIcon>
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon
-                    style={{ fontSize: "30px", fontFamily: "Anton" }}
-                  >
-                    <i /*skype icon */ />
-                    <div className="ListItem">
-                    MySkypeID
-                    </div>
-                  </ListItemIcon>
-                      <strong>
-                        Copyright © Matthew Iverson
-                        <script>
-                          document.write(new Date().getFullYear());
-                        </script>
-                        2018
-                      </strong>
-                </ListItem>
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-        </div>
+      <Paper>
+      <Typography className="contact-list">
+        <Typography>Contact Me</Typography>
+        <Typography>
+          Email: matthewiver@protonmail.com
+        </Typography>
+        <Typography>
+        Skype ID: **********
+        </Typography>
+        <Typography>Copyright © Matthew Iverson</Typography>
+      </Typography>
+      </Paper>
     );
   }
 }
