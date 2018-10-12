a353e94ab640201be931797656452b3a0b62a14b
Matthew Iverson <matthewiver@protonmail.com>
2018-10-11 20:28:29 -0500

    minimize

--- src/Sections/Biography.js
+++ src/Sections/Biography.js
@@ -10,10 +10,7 @@ import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
 import Slide from "@material-ui/core/Slide";
 import Paper from "@material-ui/core/Paper";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import EntrGridList from "../Components/UI/GridLists/EntrGridList";
 
 const styles = theme => ({
