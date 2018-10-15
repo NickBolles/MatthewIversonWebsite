02d2f3246c07127b1b108b6371720dafe89b5635
Nick Bolles <me@nickbolles.com>
2018-10-15 12:35:12 -0500

    Update AppBar.js

--- src/Components/UI/AppBar.js
+++ src/Components/UI/AppBar.js
@@ -11,11 +11,27 @@ import WorkIcon from "@material-ui/icons/Work";
 import IconButton from "@material-ui/core/IconButton";
 import SecurityIcon from "@material-ui/icons/Security";
 
-const styles = {
+const styles = (theme) => {
   root: {
     flexGrow: 1,
     width: "100%",
     position: "fixed"
+  },
+  // This is based on: https://github.com/MatthewIver/MatthewIversonWebsite/blob/master/src/App.css#L3-L17
+  appBar: {
+    background: theme.palette.primary.main
+    background: `-webkit-linear-gradient(
+      to left,
+      ${theme.palette.primary.main},
+      ${theme.palette.primary.dark}
+    )
+    `,
+    background: `linear-gradient(
+      to left,
+      ${theme.palette.primary.main},
+      ${theme.palette.primary.dark}
+    )
+    `,
   }
 };
 
@@ -24,7 +40,7 @@ function SimpleAppBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default" className="AppBar">
+      <AppBar position="static" color="default" className={classes.appBar}>
         <Toolbar>
           <Typography variant="h6" className="AppBarTitle">
             <b>Matthew Iverson</b>
